8e4a63ce173a298fee956f751038d29729380c15
jhcheon <jhcheon@realsecu.net>
2023-02-06 16:09:47 +0900

    Initial commit

---
+++ src/main/java/com/bstarbackend/bstar/BstarApplication.java
@@ -0,0 +1,13 @@
+package com.bstarbackend.bstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstarApplication.class, args);
+	}
+
+}
